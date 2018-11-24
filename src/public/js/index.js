13d1a4ca0b9be3fcd590cccf1c7dad60aea5803d
Mark Auger <mark.auger@comcast.net>
2018-11-23 22:05:00 -0600

    Wizard class

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,4 +1,5 @@
 import MotionService from './services/motion-service.js';
-
+import Wizard from './wizard.js';
 
 let motionService = new MotionService();
+let wizard = new Wizard();
--- src/public/js/wizard.js
+++ src/public/js/wizard.js
@@ -1,7 +1,26 @@
-class Wizard {
-  constructor(name,level,spells){
-    this.name = "Wizard" || name;
-    this.level = 1 || level;
-    this.spells = [] || spells;
+export default class Wizard {
+  constructor(name,level,spells,color){
+    this.name = name || "Wizard";
+    this.level = level || 1;
+    this.spells = spells || [];
+    this.color = color || "blue";
+    this.isOpponent = false;
+    this.element = this.makeElement();
+  }
+  makeElement(){
+    let element = document.createElement("IMG");
+    element.src = "src/public/img/wizards/"+this.color+".png";
+    document.body.appendChild(element);
+    return element;
+  }
+  render(){
+    if(this.isOpponent){
+
+    } else {
+
+    }
+  }
+  delete(){
+
   }
 }
