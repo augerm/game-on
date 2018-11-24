721584ee525ad9089691c7624cb9bfe70010b83d
Michael Auger <michaelauger@Michaels-MacBook-Pro.local>
2018-11-23 22:03:17 -0600

    Commit spells & stuff

--- src/public/js/services/motion-service.js
+++ src/public/js/services/motion-service.js
@@ -1,10 +1,22 @@
+
+
 export default class MotionService {
 	constructor() {
-		console.log("Hello Motion Service");
-		window.addEventListener('devicemotion', (evt) => { this.onDeviceMotion(evt) });
+		this.statusEl = document.getElementById('status');
+		this.statusEl.innerHTML = "Hello World";
+		this.initialized = false;
+		window.addEventListener('devicemotion', (event) => { this.onDeviceMotion(event) });
 	}
 
-	onDeviceMotion(evt) {
-		console.log(event.acceleration.x);
+	onDeviceMotion(event) {
+		if(event.accelerationIncludingGravity.y > 10) {
+			alert("Expelliarmus");
+		}
+		this.statusEl.innerHTML = ` <h1>
+			x: ${Math.round(event.accelerationIncludingGravity.x)}
+			y: ${Math.round(event.accelerationIncludingGravity.y)}
+			z: ${Math.round(event.accelerationIncludingGravity.z)}
+			</h1> 
+		`;
 	}
 }
